28e571224f67eb9097222ba914f3f3b564d819d7
Alberto Marín Mendoza <amarinme@uoc.edu>
2024-01-01 13:13:42 +0100

    Inicio

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,12 +5,35 @@
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
+// import * as bootstrap from "bootstrap";
 
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+const mButton = document.querySelector("#sandwich");
+const menu = document.querySelector("#menu");
+
+mButton.addEventListener(
+  "click",
+  (e) => {
+    let button;
+    if (
+      e.target.classList[0] === "sandwich__bar" 
+    )
+      button = e.target.parentElement;
+    else {
+      button = e.target;
+    }
+    let bars=button.querySelectorAll(".sandwich__bar");
+    
+    bars[0].classList.toggle("translate-y-[11px]");
+    bars[0].classList.toggle("rotate-45");
+    bars[1].classList.toggle("opacity-0");
+    bars[2].classList.toggle("-translate-y-[11px]");
+    bars[2].classList.toggle("-rotate-45");
+
+    menu.classList.toggle("hidden");
+  },
+  false,
+);
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      invert:{
+        '28': '.28',
+      },
+      sepia:{
+        '33': '.33',
+      },
+      brightness:{
+        '106': '1.06',
+      },
+      contrast:{
+        '82': '.82',
+      },
+      hueRotate:{
+        '352': '352deg',
+      },
+      saturate:{
+        '2694': '26.94',
+      }
+    },
+  },
+  plugins: [],
+}
+
